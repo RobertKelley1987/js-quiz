2f51d7cafa01e7d267c62f46bfdea00b5584a47a
Robert Kelley <robertkelley1987@gmail.com>
2022-06-22 03:03:16 -0400

    started refactoring submitQuiz function

--- scripts/submit-quiz.js
+++ scripts/submit-quiz.js
@@ -6,34 +6,30 @@ export const submitQuiz = e => {
 
     e.preventDefault();
 
-    let correctAnswers = 0; // total correct answers made by user
+    // save user responses from form to an array
     const formData = new FormData(quizForm); // user's quiz responses from form
+    let userResponses = ANSWERS.map((answer, index) => formData.get(`q${index + 1}`));
 
     // compare each user response to quiz answers
-    for (let i = 1; i <= ANSWERS.length; i++) {
-
-        // pull each answer from form data object
-        let answer = formData.get(`q${i}`);
+    for (let i = 0; i < ANSWERS.length; i++) {
 
         // test if answer is correct
-        if(answer === ANSWERS[i - 1]) {
-            // increment counter
-            correctAnswers++;
+        if(userResponses[i] === ANSWERS[i]) {
             // make checkmark visible to indicate correct answer
-            document.getElementById(`q${i}-check`).style.display = 'block';
+            document.getElementById(`q${i + 1}-check`).style.display = 'block';
         } else {
             // make x mark visible to indicate incorrect answer
-            document.getElementById(`q${i}-x`).style.display = 'block';
+            document.getElementById(`q${i + 1}-x`).style.display = 'block';
         }
 
         // add user response to HTML for user to review
-        let userResponse = document.getElementById(`q${i}-user-response`);
-        !answer ? userResponse.innerText = 'N/A' : userResponse.innerText = answer;
+        let userResponse = !userResponses[i] ? 'N/A' : userResponses[i];
+        document.getElementById(`q${i + 1}-user-response`).innerText = userResponse;
 
     }
 
     // add final score to HTML
-    quizScore.innerText = `${correctAnswers} / 10`;
+    quizScore.innerText = `${calculateScore(userResponses, ANSWERS)} / 10`;
 
     showResultsPage();
     
@@ -49,5 +45,23 @@ const showResultsPage = () => {
 
     // display post-quiz options
     postQuizButtons.style.display = 'flex';
+
+}
+
+const calculateScore = (userResponses, solutions) => {
+        
+        let correctAnswers = 0; // total correct answers made by user
+
+        // compare each user response to quiz answers
+        for (let i = 0; i < solutions.length; i++) {
+            // test if answer is correct
+            if(solutions[i] === userResponses[i]) {
+                // increment counter
+                correctAnswers++;
+
+            } 
+        }
+
+        return correctAnswers;
 
 }
